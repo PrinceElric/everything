75610c94c72f9fbbae94b88e6feb7b74b24ad72a
Elric DESSARD <elric.dessard@gmail.com>
2026-07-25 16:28:41 +0200

    update pendu_game with copilot Github!

--- pendu.py
+++ pendu.py
@@ -4,146 +4,155 @@ sys.path.append(r"C:\Users\elric\Desktop\vs code\all that")
 from tools import *  # noqa: F403
 
 
-def start():
-    global mots, mots_921, letter, letters, count_pendu, false_answers, count_pendu, pendu_etapes, word
-    mots = mots_921.copy()  # noqa: F405
-    mots, letter, letters, count_pendu, false_answers = (
-        list(filter(lambda x: True if len(x) > 5 else False, mots_921)),  # noqa: F405
-        "",
-        [],
-        0,
-        [],
-    )  # noqa: F405
-    word = mots[random.randrange(0, len(mots))]  # noqa: F405
-    word = enlever_accents(word)  # noqa: F405
-    pendu_etapes = (
-        " +---+\n |   \n     \n     \n     ",
-        " +---+\n |   \n O   \n     \n     ",
-        " +---+\n |   \n O   \n/|   \n     ",
-        " +---+\n |   \n O   \n/|\\  \n     ",
-        " +---+\n |   \n O   \n/|\\  \n/    ",
-        " +---+\n |   \n O   \n/|\\  \n/ \\  ",
-    )
+def pendu_game(mode="normal"):
+    global mots_921
 
+    def start():
+        global mots, count_down, mots_921, letter, letters, count_pendu, false_answers, pendu_etapes, word
+        mots = mots_921.copy()
+        mots, letter, letters, count_pendu, false_answers, count_down = (
+            list(filter(lambda x: True if len(x) > 5 else False, mots_921)),
+            "",
+            [],
+            0,
+            [],
+            6,
+        )
+        word = random.choice(mots)
+        word = enlever_accents(word).lower()
+        pendu_etapes = (
+            " +---+\n     \n     \n     \n     ",
+            " +---+\n |   \n     \n     \n     ",
+            " +---+\n |   \n O   \n     \n     ",
+            " +---+\n |   \n O   \n/|   \n     ",
+            " +---+\n |   \n O   \n/|\\  \n     ",
+            " +---+\n |   \n O   \n/|\\  \n/    ",
+            " +---+\n |   \n O   \n/|\\  \n/ \\  ",
+        )
 
-def show_word(mode=""):
-    if not letters and not mode:
-        print("_" * len(word))
-        return
-    elif not letters:
-        print(word[0] + ("_" * (len(word) - 1)))
-        return
-    print("".join(i if i in letters else "_" for i in word))
-    print()
+    def show_word(mode="normal"):
+        global count_down, word, letters
+        if not letters and mode == "normal":
+            print("_" * len(word))
+            return
+        elif not letters and mode == "facile":
+            letters.append(word[0])
+        elif not letters and mode == "tr_facile":
+            letters.append(word[0])
+            letters.append(word[-1])
+        elif not letters and mode == "difficile":
+            count_down = 4
+        elif mode == "debug":
+            print(f"word is {VERT_FLASH + SOULIGN2}{word}{RESET}")
+        print("".join(i if i in letters else "_" for i in word))
+        print()
 
-
-def enter_letter():
-    global letter, false_answers, word, count_pendu
-    while True:
-        if false_answers:
-            print(
-                f'{ROUGE_FLASH}False letters:   {str(false_answers).replace('[', '').replace(']', '').replace("'", '')}{RESET}'  # noqa: F405
-            )  # noqa: F405
-        letter = input("enter a letter:    ").strip().lower()
-        if letter in ["exit", "quit", "ex"]:
-            clear()  # noqa: F405
-            sys.exit()
-        elif letter == "re":
-            clear()  # noqa: F405
-            word = random.choice(mots)  # noqa: F405
-            word = enlever_accents(word)  # noqa: F405
-            main()  # noqa: F405
-        if letter == word:
-            verif(True)
-        if len(letter) != 1 or not letter.isalpha():
-            cprint("JUST ONE LETTER!", ERROR)  # noqa: F405
-            time.sleep(0.5)
+    def enter_letter():
+        global letter, false_answers, word, count_pendu
+        while True:
+            remaining_attempts = max(0, count_down - len(false_answers))
+            cprint(f"Attempts left: {remaining_attempts}", WARNING)
             if false_answers:
-                clear_lines(3)  # noqa: F405
+                print(
+                    f"{ROUGE_FLASH}False guesses:   {', '.join(false_answers)}{RESET}"
+                )
+            letter = input("enter a letter or a full word:    ").strip().lower()
+            normalized_input = enlever_accents(letter)
+
+            if normalized_input in ["exit", "quit", "ex"]:
+                clear()
+                sys.exit()
+            elif normalized_input == "re":
+                clear()
+                run()
+            elif normalized_input == word:
+                verif_game(True)
+                return
+            if len(normalized_input) != 1 or not normalized_input.isalpha():
+                cprint("JUST ONE LETTER OR A FULL WORD!", ERROR)
+                time.sleep(0.5)
+                if false_answers:
+                    clear_lines(3)
+                else:
+                    clear_lines(2)
+                continue
+
+            clear_lines(1)
+            if normalized_input in letters or normalized_input in false_answers:
+                print("Answer already gave!")
+                time.sleep(1)
+                clear()
+                return
+            if normalized_input in word:
+                print(f"enter a letter or a full word:    {VERT_FLASH}{GRAS}{normalized_input}{RESET}")
+                print(f"{normalized_input} {SUCCESS}is in the word!{RESET}")
+                letters.append(normalized_input)
             else:
-                clear_lines(2)  # noqa: F405
-            continue
-        break
-    clear_lines(1)  # noqa: F405
-    if letter in letters or letter in false_answers:
-        print("Answer already gave!")
-        time.sleep(1)
-        main()
-    if letter in word:
-        print(f"enter a letter:    {VERT_FLASH}{GRAS}{letter}{RESET}")  # noqa: F405
-        print(f"{letter} {SUCCESS}is in the word!{RESET}")  # noqa: F405
-        letters.append(letter)
+                print(f"enter a letter or a full word:    {ROUGE_FLASH}{GRAS}{normalized_input}{RESET}")
+                print(f"{normalized_input} {ERROR}not in the word!{RESET}")
+                count_pendu += 1
+                false_answers.append(normalized_input)
+            time.sleep(1)
+            return
 
-    else:
-        print(f"enter a letter:    {ROUGE_FLASH}{GRAS}{letter}{RESET}")  # noqa: F405
-        print(f"{letter} {ERROR}not in the word!{RESET}")  # noqa: F405
-        count_pendu += 1
-        false_answers.append(letter)
-    time.sleep(1)
+    def show_pendu(level: int = 0) -> None:
+        print()
+        for line in pendu_etapes[level].split("\n"):
+            print(line)
+        match level:
+            case 0:
+                clear_lines(3)
+            case 1:
+                clear_lines(2)
+            case 2 | 3:
+                clear_lines(1)
+        print()
 
+    def verif_game(full_word=False):
+        if not full_word:
+            if len(false_answers) >= count_down:
+                print(f"Answer was {FOND_VERT}{word}{RESET}")
+                cprint(
+                    f"You gave {len(false_answers)} bad answers!", ROUGE_FLASH + SOULIGN2
+                )
+                cprint(f"You had {len(letters)} good answers!", SUCCESS)
+                cprint("But...", ROUGE_FLASH)
+                cprint("You lost!", ERROR)
+                end()
+            elif len(letters) == len(set(word)):
+                print(f"Answer was {FOND_VERT}{word}{RESET}")
+                cprint("You found all the letters!", VERT_FLASH + SOULIGN2)
+                cprint("And...", VERT)
+                cprint("You won", SUCCESS)
+                end()
+            return
+        cprint(f"You guessed the word!, it was good {word}", SUCCESS)
+        print(
+            f"{VERT}You gave {len(letters)} good answers {ROUGE}and {len(false_answers)} bad answers!{RESET}"
+        )
+        cprint("You won!", SUCCESS)
+        end()
 
-def show_pendu(level: int = 0) -> None:
-    print()
-    for line in pendu_etapes[level].split("\n"):
-        print(line)
-    match level:
-        case 0:
-            clear_lines(3)  # noqa: F405
-        case 1:
-            clear_lines(2)  # noqa: F405
-        case 2 | 3:
-            clear_lines(1)  # noqa: F405
-    print()
+    def end():
+        choice = input("New round? (y/n)\n").strip().lower()
+        if choice in ["y", "yes", "o", "oui", "1"]:
+            run()
+        else:
+            sys.exit()
 
+    def main(mode="normal"):
+        while True:
+            global count_pendu
+            clear()
+            show_pendu(count_pendu)
+            verif_game()
+            show_word(mode)
+            enter_letter()
 
-def verif(full=False):
-    if not full:
-        if len(false_answers) >= 5:
-            print(f"Answer was {FOND_VERT}{word}{RESET}")  # noqa: F405
-            cprint(
-                f"You gave {len(false_answers)} bad answers!", ROUGE_FLASH + SOULIGN2
-            )  # noqa: F405
-            cprint(f"You had {len(letters)} good answers!", SUCCESS)  # noqa: F405
-            cprint("But...", ROUGE_FLASH)  # noqa: F405
-            cprint("You lost!", ERROR)  # noqa: F405
-            end()
-        elif len(letters) == len(set(word)):
-            print(f"Answer was {FOND_VERT}{word}{RESET}")  # noqa: F405
-            cprint("You found all the letters!", VERT_FLASH + SOULIGN2)  # noqa: F405
-            cprint("And...", VERT)  # noqa: F405
-            cprint("You won", SUCCESS)  # noqa: F405
-            end()
-        return
-    cprint(f"You guessed the word!, it was good {word}", SUCCESS)  # noqa: F405
-    print(
-        f"{VERT}You gave {len(letters)} good answers {ROUGE}and {len(false_answers)} bad answers!{RESET}"
-    )  # noqa: F405
-    cprint("You won!", SUCCESS)  # noqa: F405
-    end()
+    def run(mode="normal"):
+        start()
+        main(mode)
 
+    run(mode)
 
-def end():
-    choice = input("New round?\n")
-    if choice:
-        run()
-    else:
-        sys.exit()
-
-
-def main(mode=""):
-    while True:
-        global count_pendu
-        clear()  # noqa: F405
-        show_pendu(count_pendu)
-        verif()
-        print(word)  #uncomment to debug and test
-        show_word(mode)
-        enter_letter()
-
-
-def run(mode=""):
-    start()
-    main(mode)
-
-
-run("")
+pendu_game('')
--- tools.py
+++ tools.py
@@ -1128,8 +1128,9 @@ def morse(txt=''):
 
 def pendu_game(mode="normal"):
     global mots_921
+
     def start():
-        global mots, count_down, mots_921, letter, letters, count_pendu, false_answers, count_pendu, pendu_etapes, word
+        global mots, count_down, mots_921, letter, letters, count_pendu, false_answers, pendu_etapes, word
         mots = mots_921.copy()
         mots, letter, letters, count_pendu, false_answers, count_down = (
             list(filter(lambda x: True if len(x) > 5 else False, mots_921)),
@@ -1139,8 +1140,8 @@ def pendu_game(mode="normal"):
             [],
             6,
         )
-        word = mots[random.randrange(0, len(mots))]
-        word = enlever_accents(word)
+        word = random.choice(mots)
+        word = enlever_accents(word).lower()
         pendu_etapes = (
             " +---+\n     \n     \n     \n     ",
             " +---+\n |   \n     \n     \n     ",
@@ -1150,7 +1151,6 @@ def pendu_game(mode="normal"):
             " +---+\n |   \n O   \n/|\\  \n/    ",
             " +---+\n |   \n O   \n/|\\  \n/ \\  ",
         )
-
 
     def show_word(mode="normal"):
         global count_down, word, letters
@@ -1164,56 +1164,58 @@ def pendu_game(mode="normal"):
             letters.append(word[-1])
         elif not letters and mode == "difficile":
             count_down = 4
-        elif mode == 'debug':
-            print(f'word is {VERT_FLASH + SOULIGN2}{word}{RESET}')
+        elif mode == "debug":
+            print(f"word is {VERT_FLASH + SOULIGN2}{word}{RESET}")
         print("".join(i if i in letters else "_" for i in word))
         print()
-
 
     def enter_letter():
         global letter, false_answers, word, count_pendu
         while True:
+            remaining_attempts = max(0, count_down - len(false_answers))
+            cprint(f"Attempts left: {remaining_attempts}", WARNING)
             if false_answers:
                 print(
-                    f'{ROUGE_FLASH}False letters:   {str(false_answers).replace('[', '').replace(']', '').replace("'", '')}{RESET}'
+                    f"{ROUGE_FLASH}False guesses:   {', '.join(false_answers)}{RESET}"
                 )
-            letter = input("enter a letter:    ").strip().lower()
-            if letter in ["exit", "quit", "ex"]:
+            letter = input("enter a letter or a full word:    ").strip().lower()
+            normalized_input = enlever_accents(letter)
+
+            if normalized_input in ["exit", "quit", "ex"]:
                 clear()
                 sys.exit()
-            elif letter == "re":
+            elif normalized_input == "re":
                 clear()
-                word = random.choice(mots)
-                word = enlever_accents(word)
-                main()
-            if letter == word:
+                run()
+            elif normalized_input == word:
                 verif_game(True)
-            if len(letter) != 1 or not letter.isalpha():
-                cprint("JUST ONE LETTER!", ERROR)
+                return
+            if len(normalized_input) != 1 or not normalized_input.isalpha():
+                cprint("JUST ONE LETTER OR A FULL WORD!", ERROR)
                 time.sleep(0.5)
                 if false_answers:
                     clear_lines(3)
                 else:
                     clear_lines(2)
                 continue
-            break
-        clear_lines(1)
-        if letter in letters or letter in false_answers:
-            print("Answer already gave!")
+
+            clear_lines(1)
+            if normalized_input in letters or normalized_input in false_answers:
+                print("Answer already gave!")
+                time.sleep(1)
+                clear()
+                return
+            if normalized_input in word:
+                print(f"enter a letter or a full word:    {VERT_FLASH}{GRAS}{normalized_input}{RESET}")
+                print(f"{normalized_input} {SUCCESS}is in the word!{RESET}")
+                letters.append(normalized_input)
+            else:
+                print(f"enter a letter or a full word:    {ROUGE_FLASH}{GRAS}{normalized_input}{RESET}")
+                print(f"{normalized_input} {ERROR}not in the word!{RESET}")
+                count_pendu += 1
+                false_answers.append(normalized_input)
             time.sleep(1)
-            main()
-        if letter in word:
-            print(f"enter a letter:    {VERT_FLASH}{GRAS}{letter}{RESET}")
-            print(f"{letter} {SUCCESS}is in the word!{RESET}")
-            letters.append(letter)
-
-        else:
-            print(f"enter a letter:    {ROUGE_FLASH}{GRAS}{letter}{RESET}")
-            print(f"{letter} {ERROR}not in the word!{RESET}")
-            count_pendu += 1
-            false_answers.append(letter)
-        time.sleep(1)
-
+            return
 
     def show_pendu(level: int = 0) -> None:
         print()
@@ -1227,7 +1229,6 @@ def pendu_game(mode="normal"):
             case 2 | 3:
                 clear_lines(1)
         print()
-
 
     def verif_game(full_word=False):
         if not full_word:
@@ -1254,14 +1255,12 @@ def pendu_game(mode="normal"):
         cprint("You won!", SUCCESS)
         end()
 
-
     def end():
-        choice = input("New round?\n")
-        if choice:
+        choice = input("New round? (y/n)\n").strip().lower()
+        if choice in ["y", "yes", "o", "oui", "1"]:
             run()
         else:
             sys.exit()
-
 
     def main(mode="normal"):
         while True:
@@ -1272,11 +1271,9 @@ def pendu_game(mode="normal"):
             show_word(mode)
             enter_letter()
 
-
     def run(mode="normal"):
         start()
         main(mode)
-
 
     run(mode)
 
